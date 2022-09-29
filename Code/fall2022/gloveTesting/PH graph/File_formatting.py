f4bc915b63cde04b8664d536dedb7722847ed325
jaldridge <89814068+jaldridg@users.noreply.github.com>
2022-09-29 19:08:36 -0400

    Started writing real time plotting

--- Code/fall2022/gloveTesting/PH graph/File_formatting.py
+++ Code/fall2022/gloveTesting/PH graph/File_formatting.py
@@ -1,55 +1,55 @@
 import matplotlib.pyplot as plt
 
-with open('Arduino_Outputs') as f:
-    lines = f.readlines()
+# with open('Arduino_Outputs') as f:
+#     lines = f.readlines()
 
-# Remove \n 
-a = lines[0].split("\\n")
-p = []
-# Remove 'b's
-for i in a:
-    p.append(i.replace("\'b\'",""))
-while p[0][0:1] != "R":
-    p.pop(0)
+raw_readings = []
+converted_readings = []  
 
-raw = []
-converted = []    
-for i in range(0,len(p)):
-    #converted
-    if (i % 2 == 1):
-        p[i] = p[i][17:].strip()
-        converted.append(p[i])
-    #raw
+# Cleans an unprocessed line from the output and adds it to its data
+def add_data(input):
+    # Remove '\n'
+    # lines = input.split("\\n")
+    # cleaned_lines = []
+    # Remove 'b's
+    # for line in lines:
+    # cleaned_line = input.replace("b\'","")
+    # cleaned_line = input.replace("\\n'","")
+    
+    cleaned_line = input[2:-3]
+    if(len(cleaned_line) <= 5):
+        return
+    # Not sure what this does
+    # while cleaned_lines[0][0:1] != "R":
+    #     cleaned_lines.pop(0)
+
+    # Take substrings of the values to include only the numbers
+        # Converted values
+    # print(cleaned_line)
+    if (cleaned_line[0] == "C"):
+        cleaned_line = cleaned_line[17:].strip()
+        print(cleaned_line)
+        if cleaned_line.isdigit():
+            cleaned_line = float(cleaned_line)
+            converted_readings.append(cleaned_line)
+    # Raw values
     else:
-        p[i] = p[i][10:].strip()
-        raw.append(p[i])
+        cleaned_line = cleaned_line[10:].strip()
+        if cleaned_line.isdigit():
+            cleaned_line = float(cleaned_line)
+            raw.append(cleaned_line)
+        
 
-# Convert raw
-k = 0
-for i in raw:
-    if (not i.isdigit()):
-        raw.pop(k)
-        k += 1
 
-# Convert converted
-k = 0
-for i in converted:
-    if (not i.isdigit()):
-        converted.pop(k)
-        k += 1
+# Make a graph using the values
+def make_graph():  
+    timelist = range(0, len(converted))
+    timelist = [time/3 for time in timelist]
+    plt.plot(timelist, converted)
 
-print(raw)
-print(converted)
-
-converted = [float(convert) for convert in converted]
-
-timelist = range(0, len(converted))
-timelist = [time/3 for time in timelist]
-plt.plot(timelist, converted)
-  
-#Graph of converted values vs. time
-plt.xlabel("time")
-plt.ylabel("converted values")
-plt.title('time vs. converted values')
-plt.legend()
-plt.show()
+    #Graph of converted values vs. time
+    plt.xlabel("time")
+    plt.ylabel("converted values")
+    plt.title('time vs. converted values')
+    plt.legend()
+    plt.show()
--- Code/fall2022/gloveTesting/PH graph/PH-text.py
+++ Code/fall2022/gloveTesting/PH graph/PH-text.py
@@ -1,9 +1,15 @@
-#write if statements to save certain measurements to a text file
+# Write if statements to save certain measurements to a text file
 import serial
-ser = serial.Serial('COM4', 9800, timeout=1)
+import File_formatting as ff
+foundR  = False
+ser = serial.Serial('COM3', 9800, timeout=1)
 with open('Arduino_Outputs', 'w') as f:
     while True:
-        a = ser.readline()
-        print(a)
-        a = str(a)
-        f.write(a)
+        line = ser.readline()
+        print(line)
+        f.write(str(line))
+        line = str(line)
+        if(foundR ==True or line[2] == "R"):
+            foundR = True
+            ff.add_data(line)
+            # ff.make_graph()
