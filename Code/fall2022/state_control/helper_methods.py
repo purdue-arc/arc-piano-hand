0c6c08ceb8cad5ee101336fdd2d0680ea99acbe1
Revanth Krishna Senthilkumaran <revanthsenthil@gmail.com>
2022-10-01 18:29:00 -0400

    commit state_updates from revanth's PC

--- Code/fall2022/state_control/helper_methods.py
+++ Code/fall2022/state_control/helper_methods.py
@@ -1,9 +1,46 @@
 def distanceInWhiteKeys(referenceKeyNumber, keyNumberToCalculate):
+    # MIDI note number 0 = C octave -2 if 60 if C octave 3
+    # Adjust to common range
     while(referenceKeyNumber > 12):
         referenceKeyNumber -= 12
         keyNumberToCalculate -= 12
+
+    # Count number of octaves difference
     octaves = int(keyNumberToCalculate - referenceKeyNumber) / 12
     octAdjustKeyNumber = keyNumberToCalculate - 12*octaves
-    
 
-def state_update()
+    isWhite = {
+        0 : True,
+        1 : False,
+        2 : True,
+        3 : False,
+        4 : True,
+        5 : True,
+        6 : False,
+        7 : True,
+        8 : False,
+        9 : True,
+        10 : False,
+        11 : True
+    }
+
+    numWhiteKeys = 0
+    tempKeyNumber = referenceKeyNumber
+
+    while(tempKeyNumber != keyNumberToCalculate):
+        tempKeyNumber += 1
+        if(isWhite(tempKeyNumber % 12)):
+            numWhiteKeys += 1
+    
+    if keyNumberToCalculate % 12 != referenceKeyNumber % 12:
+        numWhiteKeys += 0.5
+
+    return numWhiteKeys
+
+
+
+
+def 
+
+def state_update():
+    
--- Code/fall2022/state_control/state_test.py
+++ Code/fall2022/state_control/state_test.py
@@ -2,6 +2,6 @@
 # Base finger: Middle
 # Base loc: C3, D3, E3, F3, G3
 
-
-
-
+# G3 : 
+# G#3: 
+# Gb3: 
