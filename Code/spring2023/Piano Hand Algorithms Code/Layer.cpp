22e923ac43eb74ab5705668f29c8ee6fc89ab1fa
TPNxl <timmynadolsky@gmail.com>
2023-02-02 19:16:12 -0500

    Added layer error checks

--- Code/spring2023/Piano Hand Algorithms Code/Layer.cpp
+++ Code/spring2023/Piano Hand Algorithms Code/Layer.cpp
@@ -7,7 +7,11 @@ Layer::Layer() {
 
 int Layer::add(Node node)
 {
+	if (std::count(nodes.begin(), nodes.end(), node)) {
+		throw std::invalid_argument("Node already in Layer");
+	}
 	nodes.push_back(node);
+	return 0;
 }
 
 int Layer::remove(Node node)
@@ -15,6 +19,8 @@ int Layer::remove(Node node)
 	for(auto i = 0; i < sizeof(nodes); i++) {
 		if (&nodes[i] == &node) {
 			nodes.erase(nodes.begin()+i);
+			return 0;
 		}
 	}
+	throw std::invalid_argument("Node not in Layer");
 }
