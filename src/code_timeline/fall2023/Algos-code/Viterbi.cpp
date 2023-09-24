34d54a28587695b132734d009aa06ae21baf418e
unknown <timmynadolsky@gmail.com>
2023-09-23 21:17:48 -0400

    Added comments to basic Viterbi implementation

--- src/code_timeline/fall2023/Algos-code/Viterbi.cpp
+++ src/code_timeline/fall2023/Algos-code/Viterbi.cpp
@@ -5,24 +5,33 @@
 #include "Viterbi.h"
 #include <malloc.h>
 #include <iostream>
+#include <vector>
 
-int cost(int start_i, int start_j, int end_i, int end_j) {
-    int trellis[4][3] = {{2, 4, 3},
-                         {3, 2, 4},
-                         {5, 3, 2},
-                         {2, 3, 4}};
+// Sample trivial cost function (cost is the value of the end node)
+int cost(std::vector<std::vector<int>> trellis, int start_i, int start_j, int end_i, int end_j) {
     return trellis[end_i][end_j];
 }
 
 int main() {
-    int trellis[4][3] = {{2, 4, 3},
-                         {3, 2, 4},
-                         {5, 3, 2},
-                         {2, 3, 4}};
+    // Data initialization
+    std::vector<std::vector<int>> trellis =
+    {
+        {1, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 0, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 0, 1, 1},
+        {1, 1, 1, 0, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 0, 1, 1},
+        {1, 1, 1, 1, 0, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 1},
+        {1, 1, 1, 0, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 0},
+        {1, 1, 1, 1, 0, 1, 1, 1},
+        {1, 1, 1, 0, 1, 1, 1, 1}
+    };
+    int n_layers = 11;
+    int layer_size = 8;
 
-    int n_layers = 4;
-    int layer_size = 3;
-
+    // Make arrays for Viterbi
     int dist[n_layers][layer_size];
     int prev[n_layers][layer_size];
     for (int i = 0; i < n_layers; i++) {
@@ -32,26 +41,30 @@ int main() {
         }
     }
 
+    // Run Viterbi
     for (int i = 0; i < n_layers; i++) {
         for (int j = 0; j < layer_size; j++) {
             int min_dist = INT_MAX;
             int min_prev = INT_MIN;
-            if (i == 0) {
+            if (i == 0) {   // If first layer (to avoid OOB accesses)
                 min_dist = trellis[i][j];
             }
             else {
                 for (int k = 0; k < layer_size; k++) {
-                    int curr_dist = cost(i-1, k, i, j) + dist[i-1][k];
+                    // Check our distance recurrence and update min distances if better option
+                    int curr_dist = cost(trellis, i - 1, k, i, j) + dist[i - 1][k];
                     if (curr_dist < min_dist) {
                         min_dist = curr_dist;
                         min_prev = k;
                     }
                 }
             }
+            // Add values to array
             dist[i][j] = min_dist;
             prev[i][j] = min_prev;
         }
     }
+    // Find the minimum-distance item in the final layer
     int min = INT_MAX;
     int idx = 0;
     for (int i = 0; i < layer_size; i++) {
@@ -60,15 +73,16 @@ int main() {
             idx = i;
         }
     }
+
+    // Print results
     std::cout << "Final distance: " << dist[n_layers - 1][idx] << "\n";
     for (int i = n_layers - 1; i >= 0; i--) {
+        // Use cout as a pseudo-stack
         std::cout << "(" << i << "," << idx << ") -> ";
         idx = prev[i][idx];
     }
     std::cout << "end\n\n";
-
     std::cout << "Array contents:\n\n";
-
     std::cout << "dist:\n";
     for (int i = 0; i < n_layers; i++) {
         for (int j = 0; j < layer_size; j++) {
@@ -77,11 +91,15 @@ int main() {
         std::cout << "\n";
     }
     std::cout << "\n";
-
     std::cout << "prev:\n";
     for (int i = 0; i < n_layers; i++) {
         for (int j = 0; j < layer_size; j++) {
-            std::cout << prev[i][j] << " ";
+            if (prev[i][j] == INT_MIN) {
+                std::cout << "n" << " ";
+            }
+            else {
+                std::cout << prev[i][j] << " ";
+            }
         }
         std::cout << "\n";
     }
