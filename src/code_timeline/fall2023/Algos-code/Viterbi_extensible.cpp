7dc738a2674639572c9fea643fdb5f8257d57b17
unknown <timmynadolsky@gmail.com>
2023-09-30 17:04:54 -0400

    Added Viterbi_extensible

--- src/code_timeline/fall2023/Algos-code/Viterbi_extensible.cpp
+++ src/code_timeline/fall2023/Algos-code/Viterbi_extensible.cpp
@@ -3,42 +3,21 @@
 //
 
 #include <malloc.h>
-#include <iostream>
+#include <cfloat>
 #include <vector>
 
 #include "phSpace.h"
 using namespace phSpace;
 
-// Sample trivial cost function (cost is the value of the end node)
-int cost(std::vector<std::vector<int>> trellis, int start_i, int start_j, int end_i, int end_j) {
-    return trellis[end_i][end_j];
-}
-
-int main() {
-    // Data initialization
-    std::vector<std::vector<int>> trellis =
-    {
-        {1, 0, 1, 1, 1, 1, 1, 1},
-        {1, 1, 0, 1, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 0, 1, 1},
-        {1, 1, 1, 0, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 0, 1, 1},
-        {1, 1, 1, 1, 0, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 0, 1},
-        {1, 1, 1, 0, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 1, 0},
-        {1, 1, 1, 1, 0, 1, 1, 1},
-        {1, 1, 1, 0, 1, 1, 1, 1}
-    };
-    int n_layers = 11;
-    int layer_size = 8;
-
+std::vector<int> Viterbi::run_algo(int n_layers, int layer_size,
+                                   double (*lookup)(int start_i, int start_j, int end_i, int end_j)) {
+    double trellis[n_layers][layer_size];
     // Make arrays for Viterbi
-    int dist[n_layers][layer_size];
+    double dist[n_layers][layer_size];
     int prev[n_layers][layer_size];
     for (int i = 0; i < n_layers; i++) {
         for (int j = 0; j < layer_size; j++) {
-            dist[i][j] = INT_MAX;
+            dist[i][j] = DBL_MAX;
             prev[i][j] = INT_MIN;
         }
     }
@@ -46,7 +25,7 @@ int main() {
     // Run Viterbi
     for (int i = 0; i < n_layers; i++) {
         for (int j = 0; j < layer_size; j++) {
-            int min_dist = INT_MAX;
+            double min_dist = DBL_MAX;
             int min_prev = INT_MIN;
             if (i == 0) {   // If first layer (to avoid OOB accesses)
                 min_dist = trellis[i][j];
@@ -54,7 +33,7 @@ int main() {
             else {
                 for (int k = 0; k < layer_size; k++) {
                     // Check our distance recurrence and update min distances if better option
-                    int curr_dist = cost(trellis, i - 1, k, i, j) + dist[i - 1][k];
+                    double curr_dist = (*lookup)(i - 1, k, i, j) + dist[i - 1][k];
                     if (curr_dist < min_dist) {
                         min_dist = curr_dist;
                         min_prev = k;
@@ -67,7 +46,7 @@ int main() {
         }
     }
     // Find the minimum-distance item in the final layer
-    int min = INT_MAX;
+    auto min = DBL_MAX;
     int idx = 0;
     for (int i = 0; i < layer_size; i++) {
         if (dist[n_layers - 1][i] < min) {
@@ -76,33 +55,12 @@ int main() {
         }
     }
 
-    // Print results
-    std::cout << "Final distance: " << dist[n_layers - 1][idx] << "\n"; 
+    std::vector<int> vals;
     for (int i = n_layers - 1; i >= 0; i--) {
-        // Use cout as a pseudo-stack
-        std::cout << "(" << i << "," << idx << ") -> ";
+        vals.insert(vals.begin(), idx);
         idx = prev[i][idx];
     }
-    std::cout << "end\n\n";
-    std::cout << "Array contents:\n\n";
-    std::cout << "dist:\n";
-    for (int i = 0; i < n_layers; i++) {
-        for (int j = 0; j < layer_size; j++) {
-            std::cout << dist[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
-    std::cout << "prev:\n";
-    for (int i = 0; i < n_layers; i++) {
-        for (int j = 0; j < layer_size; j++) {
-            if (prev[i][j] == INT_MIN) {
-                std::cout << "n" << " ";
-            }
-            else {
-                std::cout << prev[i][j] << " ";
-            }
-        }
-        std::cout << "\n";
-    }
+
+    return vals;
 }
+
--- src/code_timeline/fall2023/Algos-code/test_case_generator.cpp
+++ src/code_timeline/fall2023/Algos-code/test_case_generator.cpp
@@ -1,3 +1,22 @@
 //
 // Created by TPNml on 9/30/2023.
 //
+
+#include <random>
+#include <vector>
+#include <phSpace.h>
+
+using namespace phSpace;
+
+int main() {
+    int x[30] = { 0 };
+    for (int & i : x) {
+        i = std::rand() % 36 + 48;
+    }
+    std::vector<int> v(x, x + sizeof x / sizeof x[0]);
+    try {
+        std::vector<int> output = initial_idea::minMvmtsForNotes(v);
+    } catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
