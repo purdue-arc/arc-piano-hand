358c46fca066b551512378d73a582d35ca172f36
unknown <timmynadolsky@gmail.com>
2023-10-07 16:17:55 -0400

    Add Viterbi changes

--- src/code_timeline/fall2023/Algos-code/Viterbi.cpp
+++ src/code_timeline/fall2023/Algos-code/Viterbi.cpp
@@ -9,34 +9,37 @@
 #include "phSpace.h"
 using namespace phSpace;
 
+std::vector<std::vector<int>> trellis = {
+    {1, 0, 1, 1, 1, 1, 1, 1},
+    {1, 1, 0, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 0, 1, 1},
+    {1, 1, 1, 0, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 0, 1, 1},
+    {1, 1, 1, 1, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 0, 1},
+    {1, 1, 1, 0, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 0},
+    {1, 1, 1, 1, 0, 1, 1, 1},
+    {1, 1, 1, 0, 1, 1, 1, 1}
+};
+
 // Sample trivial cost function (cost is the value of the end node)
-int cost(std::vector<std::vector<int>> trellis, int start_i, int start_j, int end_i, int end_j) {
+int cost(int start_i, int start_j, int end_i, int end_j) {
     return trellis[end_i][end_j];
 }
 
-int main() {
-    // Data initialization
-    std::vector<std::vector<int>> trellis =
-    {
-        {1, 0, 1, 1, 1, 1, 1, 1},
-        {1, 1, 0, 1, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 0, 1, 1},
-        {1, 1, 1, 0, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 0, 1, 1},
-        {1, 1, 1, 1, 0, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 0, 1},
-        {1, 1, 1, 0, 1, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 1, 0},
-        {1, 1, 1, 1, 0, 1, 1, 1},
-        {1, 1, 1, 0, 1, 1, 1, 1}
-    };
-    int n_layers = 11;
-    int layer_size = 8;
+int layerSize(int end_i) {
+    return trellis[end_i].size();
+}
 
+void ViterbiAlgo(int (*cost)(int, int, int, int), int n_layers) {
     // Make arrays for Viterbi
-    int dist[n_layers][layer_size];
-    int prev[n_layers][layer_size];
+    int *dist[n_layers];
+    int *prev[n_layers];
     for (int i = 0; i < n_layers; i++) {
+        int layer_size = layerSize(i);
+        dist[i] = new int[layer_size];
+        prev[i] = new int[layer_size];
         for (int j = 0; j < layer_size; j++) {
             dist[i][j] = INT_MAX;
             prev[i][j] = INT_MIN;
@@ -45,6 +48,7 @@ int main() {
 
     // Run Viterbi
     for (int i = 0; i < n_layers; i++) {
+        int layer_size = layerSize(i);
         for (int j = 0; j < layer_size; j++) {
             int min_dist = INT_MAX;
             int min_prev = INT_MIN;
@@ -54,7 +58,7 @@ int main() {
             else {
                 for (int k = 0; k < layer_size; k++) {
                     // Check our distance recurrence and update min distances if better option
-                    int curr_dist = cost(trellis, i - 1, k, i, j) + dist[i - 1][k];
+                    int curr_dist = cost(i - 1, k, i, j) + dist[i - 1][k];
                     if (curr_dist < min_dist) {
                         min_dist = curr_dist;
                         min_prev = k;
@@ -69,7 +73,7 @@ int main() {
     // Find the minimum-distance item in the final layer
     int min = INT_MAX;
     int idx = 0;
-    for (int i = 0; i < layer_size; i++) {
+    for (int i = 0; i < layerSize(n_layers-1); i++) {
         if (dist[n_layers - 1][i] < min) {
             min = dist[n_layers - 1][i];
             idx = i;
@@ -80,13 +84,14 @@ int main() {
     std::cout << "Final distance: " << dist[n_layers - 1][idx] << "\n"; 
     for (int i = n_layers - 1; i >= 0; i--) {
         // Use cout as a pseudo-stack
-        std::cout << "(" << i << "," << idx << ") -> ";
+        std::cout << "(" << i << "," << idx << ") <- ";
         idx = prev[i][idx];
     }
-    std::cout << "end\n\n";
+    std::cout << "start\n\n";
     std::cout << "Array contents:\n\n";
     std::cout << "dist:\n";
     for (int i = 0; i < n_layers; i++) {
+        int layer_size = layerSize(i);
         for (int j = 0; j < layer_size; j++) {
             std::cout << dist[i][j] << " ";
         }
@@ -95,6 +100,7 @@ int main() {
     std::cout << "\n";
     std::cout << "prev:\n";
     for (int i = 0; i < n_layers; i++) {
+        int layer_size = layerSize(i);
         for (int j = 0; j < layer_size; j++) {
             if (prev[i][j] == INT_MIN) {
                 std::cout << "n" << " ";
@@ -105,4 +111,8 @@ int main() {
         }
         std::cout << "\n";
     }
+}
+
+int main() {
+    ViterbiAlgo(&cost, 11);
 }
