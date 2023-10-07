2ce942565600853f9653d88b6090b378b1ce6057
unknown <timmynadolsky@gmail.com>
2023-10-06 21:16:14 -0400

    test code

--- src/code_timeline/fall2023/Algos-code/simple_code.cpp
+++ src/code_timeline/fall2023/Algos-code/simple_code.cpp
@@ -5,6 +5,44 @@
 #include "phSpace.h"
 using namespace phSpace;
 
+int cool_thing() {
+    int **x = (int **) malloc(20 * sizeof(int *));
+    for (int i = 0; i < 20; i++) {
+        *(x + i) = (int *) malloc(i * sizeof(int));
+    }
+    for (int i = 0; i <= 20; i++) {
+        for (int j = 0; j < i; j++) {
+            x[i][j] = i + j;
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < i; j++) {
+            sum += *(*(x+i)+j);
+        }
+    }
+    for (int i = 0; i < 20; i++) {
+        free(*(x + i));
+        *(x + i) = nullptr;
+    }
+    free(x);
+    x = nullptr;
+    std::cout << sum << std::endl;
+    return 0;
+}
+
 int main() {
+    FILE *fp = fopen("cmake-build-debug/cool_numbers.txt", "w+");
+    if (fp == nullptr) {
+        return 0;
+    }
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < i; j++) {
+            fprintf(fp, "%d ", j);
+        }
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+    fp = nullptr;
     return 0;
 }
