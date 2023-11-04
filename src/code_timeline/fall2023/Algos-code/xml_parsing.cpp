bbb00a8cefe00e10e075a799032140f2d970152b
unknown <timmynadolsky@gmail.com>
2023-11-04 15:57:57 -0400

    CMakeLists

--- src/code_timeline/fall2023/Algos-code/xml_parsing.cpp
+++ src/code_timeline/fall2023/Algos-code/xml_parsing.cpp
@@ -8,10 +8,9 @@
 using namespace phSpace;
 
 void print_notes(rapidxml::xml_node<> *node) {
-    for (node = node->first_node(); node != NULL; node = node->next_sibling()) {
+    for (node = node->first_node(); node != nullptr; node = node->next_sibling()) {
         if (node->type() == rapidxml::node_element) //check node type
-        {
-            std::string x = node->value();
+        {std::string x = node->value();
            std::cout << node->name() << ": " << x << std::endl;
            print_notes(node);
         }
@@ -20,7 +19,7 @@ void print_notes(rapidxml::xml_node<> *node) {
 
 // Allows you to send nodes for
 void handlenode(rapidxml::xml_node<> *node, std::string x) {
-    for (node = node->first_node(); node != NULL; node = node->next_sibling()) {
+    for (node = node->first_node(); node != nullptr; node = node->next_sibling()) {
         if (node->type() == rapidxml::node_element) //check node type
         {
             if (x == node->name()) {
@@ -35,13 +34,14 @@ void handlenode(rapidxml::xml_node<> *node, std::string x) {
 
 // Allows you to send nodes for
 rapidxml::xml_node<> *search_for_node(rapidxml::xml_node<> *node, std::string x) {
-    for (node = node->first_node(); node != NULL; node = node->next_sibling()) {
+    for (node = node->first_node(); node != nullptr; node = node->next_sibling()) {
         if (node->type() == rapidxml::node_element) //check node type
         {
             if (x == node->name()) {
-               return node;
+                std::cout << node->name() << ": " << node->value() << std::endl;
+                return node;
             }
-            handlenode(node, x);
+            search_for_node(node, x);
         }
     }
     return nullptr;
@@ -54,25 +54,38 @@ Note * parse_note(rapidxml::xml_node<> *node) { // Node is the root of the tree 
     // And duration
     // Store each of these in a variable
     // You can use the search_for_node function above which searches for a given tag in the tree
-    std::string note_name = search_for_node(node, "name")->value();
+    rapidxml::xml_node<> *step = search_for_node(node, "step");
+    if (step == nullptr) {
+        std::cout << "bad" << std::endl;
+    }
+    //std::string note_name = search_for_node(node, "step")->value();
+    /*
     std::string octave = search_for_node(node, "octave")->value();
     std::string alter = search_for_node(node, "alter")->value();
     std::string duration = search_for_node(node, "duration")->value();
+     */
 
-    // Then call the Note constructor with these values and return the resulting Note
-    Note *parsedNote = new Note(note_name, octave, alter, duration, 0);
+    //std::cout << note_name /*<< " " << octave << " " << alter << duration*/ << std::endl;
+    // Then call the Note constructor with these values and return the resulting Note*/
+    //Note *parsedNote = new Note(note_name, octave, alter, duration, 0);
+    Note *parsedNote = new Note(0,0);
     return parsedNote;
 }
 
 std::vector<Note *> find_notes(rapidxml::xml_node<> *node) {
     std::vector<Note *> notes{};
+    std::string x("note");
     for (node = node->first_node(); node != NULL; node = node->next_sibling()) {
         if (node->type() == rapidxml::node_element) //check node type
         {
-            if ("note" == node->name()) {
-               notes.push_back(parse_note(node)); //add to the end
+            if (x == node->name()) {
+                std::cout << "hi";
+                //print_notes(node);
+                notes.push_back(parse_note(node)); //add to the end
+            } else {
+                std::vector<Note *> output = find_notes(node);
+                notes.insert(notes.end(), output.begin(), output.end());
             }
-            find_notes(node);
         }
     }
     return notes;
@@ -111,10 +124,13 @@ int main() {
     doc.parse<0>(xmlFile.data());
 
     rapidxml::xml_node<> *title = doc.first_node();
-    // handlenode(title, "note");
+    handlenode(title, "note");
 
+    std::cout << search_for_node(title, "note")->name() << std::endl;
 
-    for(auto note : find_notes(title)) {
+    std::vector<Note *> notes = find_notes(title);
+
+    for(auto note : notes) {
         std::cout << note->toString() << std::endl;
     }
 
