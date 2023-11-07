4e7ad93007250863701fafcaff1b69282732a2aa
YoshiFrancis <101031001+YoshiFrancis@users.noreply.github.com>
2023-11-06 22:33:43 -0500

    Add files via upload
    Removed unnecessary code and comments. All this code does is simulate the PID system with a motor. As the potentiometer gets closer to a specific voltage, since no angle conversion equation has been implemented yet, the motor will slow down until it reaches a certain threshold (+/- 100 voltage within target).

--- src/code_timeline/fall2023/electronics/ChangeOneFingerTest.ino
+++ src/code_timeline/fall2023/electronics/ChangeOneFingerTest.ino
@@ -1,15 +1,8 @@
-constexpr int fingerCount {5};
+constexpr int fingerCount = 1;
 const int fingerPotPins[1] = {A1};
-const int fingerPins[fingerCount] = {1, 2, 3, 4, 5};
-const String getPotValsSignal = "p";
+const int fingerPins[fingerCount] = {1};
 int currPotVal {0};
 int currAngles[fingerCount]; 
-
-int currTime {0};
-int positionFingerPrevTime {0};
-int getPotValsPrevTime {0};
-// 0 means no delay
-constexpr int dt {0};
 
 int speedPin = 10;
 int directionPinOne = 9;
@@ -38,60 +31,30 @@ void setup() {
   delay(100);
 }
 
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  currTime = millis();
-  // if (currTime - getPotValsPrevTime >= dt)
-  // {
-  //   getPotValsPrevTime = currTime;
-  //   if (Serial.available() > 0 && Serial.readString().equals(getPotValsSignal)) {
-  //     Serial.println("I GOT THE SIGNAL");
-  //     currAngles[0] = analogRead(fingerPotPins[0]);
-  //     Serial.println(currAngles[0]);
-
-  //     // this is only assumiing there is one finger (one target angle)
-  //     int targetAngle = Serial.readString().toInt();
-  //     Serial.println(targetAngle);
-  //     positionOneFinger(targetAngle, currAngles[0], motorPin);
-  //   }
-  // }
   currAngles[0] = analogRead(fingerPotPins[0]);
-  int targetAngle = 600;
-  Serial.println(currAngles[0]);
+  const int targetAngle = 600;
   positionOneFinger(targetAngle, currAngles[0], motorPin);
-
-  
-  // if (currTime - getPotValsPrevTime <= 20) 
-  //   positionAllFingers(getTargetAngles(), currAngles, fingerPins);
   delay(1000);
 }
 
-// will position finger towards the target angle wanted
-
+// will position fingers toward the target angles wanted
 void positionAllFingers(int targetAngles[], int currAngles[], int fingerPins[])
 {
   for (int i = 0; i < fingerCount; ++i)
-  {
     positionOneFinger(targetAngles[i], currAngles[i], fingerPins[i]);
-  }
 }
 
 void positionOneFinger(int targetAngle, int currAngle, int fingerPin)
 {
   const int threshold {100};
-  if (abs(currAngle - targetAngle) < threshold) 
-  {
-    // does not move finger
+  const int angle = currAngle - targetAngle;
+  if (abs(angle) < threshold) 
     return;
-  }
-  if (currAngle < targetAngle)
-  // moves finger in the positive direction
-    moveFinger(100, fingerPin, targetAngle);
-  else if (currAngle > targetAngle ) 
-  // moves finger in the negative direction 
-    moveFinger(-100, fingerPin, targetAngle);
-  return; 
+  if (angle < 0)
+    moveFinger(-angle, fingerPin, targetAngle); // moves finger in the negative direction
+  else if (angle > 0) 
+    moveFinger(angle, fingerPin, targetAngle); // moves finger in the positive direction
 }
 
 // will move the fingers by directly accessing motors
@@ -99,42 +62,32 @@ void moveFinger(int voltage, const int fingerPin, const int targetAngle)
 {
   if (abs(voltage) > 255)
     voltage = 255;
-  Serial.println(voltage);
 
   motSpeed = abs(voltage);
   const int threshold {100};
-  if (voltage == 0)
+  if (voltage > 0) 
   {
     digitalWrite(directionPinOne, HIGH);
     digitalWrite(directionPinTwo, LOW);
-    analogWrite(speedPin, 0);
-    Serial.println("DONT MOVE");
-    return;
-  }
-  else if (voltage > 0) 
-  {
-    digitalWrite(directionPinOne, HIGH);
-    digitalWrite(directionPinTwo, LOW);
-    analogWrite(speedPin, motSpeed);
   }
   else if (voltage < 0)
   {
     digitalWrite(directionPinOne, LOW);
     digitalWrite(directionPinTwo, HIGH);
-    analogWrite(speedPin, motSpeed);
   }
-  int angle = getCurrAngle(fingerPin) - targetAngle;
+  analogWrite(speedPin, motSpeed);
+
+  const int angle = getCurrAngle(fingerPin) - targetAngle;
 
   if (abs(angle) > threshold)
   {      
     if (angle < 0)
       moveFinger(angle / 2, fingerPin, targetAngle);
     else 
-      moveFinger(angle / 2, fingerPin, targetAngle);
+      moveFinger(-angle / 2, fingerPin, targetAngle);
   }
   else 
     moveFinger(0, fingerPin, targetAngle);
-  return;
 }
 
 
@@ -142,6 +95,7 @@ void moveFinger(int voltage, const int fingerPin, const int targetAngle)
 int convertPotValToAngle(int potVal)
 {
   // convert the potval into angle below
+  // the formula has not yet been implemented
   return potVal;
 }
 
@@ -154,6 +108,6 @@ int getCurrAngle(int fingerPin)
 int* getTargetAngles()
 {
   // access angles from computer and return them
-  int tmp[fingerCount] = {1, 2, 3, 4, 5};
+  int tmp[fingerCount] = {1};
   return tmp;
 }
