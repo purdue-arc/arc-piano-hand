c31a90deda9a757c71c5587a80bb66494e99f855
Iris <iris.m.gong@gmail.com>
2024-02-22 20:06:19 -0500

    Increased resolution and dpi

--- src/code_timeline/spring2024/camScannerAuto.py
+++ src/code_timeline/spring2024/camScannerAuto.py
@@ -3,11 +3,10 @@ import cv2
 from skimage.filters import threshold_local #check this 
 import numpy as np
 import argparse
-import cv2
 import imutils
 import pyrealsense2 as rs
 import time
-import numpy
+from PIL import Image
 
 def order_points(pts):
 	# initialzie a list of coordinates that will be ordered
@@ -73,13 +72,20 @@ def equalArray(array1, array2):
     for i in range(len(array1)):
         for j in range(1):
             for k in range(2):
-                print(abs(array1[i][j][k] - array2[i][j][k]))
                 if abs(array1[i][j][k] - array2[i][j][k]) > 50:
                     same = False
     return same
 
 print("test")
-video = cv2.VideoCapture(0,cv2.CAP_DSHOW) # change for camera 
+video = cv2.VideoCapture(0, cv2.CAP_DSHOW) # change for camera 
+
+codec = 0x47504A4D  # MJPG
+video.set(cv2.CAP_PROP_FPS, 30.0)
+video.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('m','j','p','g'))
+video.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M','J','P','G'))
+video.set(cv2.CAP_PROP_FRAME_WIDTH, 1920*2)
+video.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080*2)
+
 if not video.isOpened():
 	print("Failed to open camera")
 	exit()
@@ -155,24 +161,33 @@ while True:
 	# cv2.waitKey(0)
 	# cv2.destroyAllWindows() 
  
-	if consec == 100:	# adjust waitkey() to adjust frame rate
+	if consec == 50:	# adjust waitkey() to adjust frame rate
 		# apply the four point transform to obtain a top-down
 		# view of the original image
 		warped = four_point_transform(orig, screenCnt.reshape(4, 2))  # * ratio
 		# convert the warped image to grayscale, then threshold it
 		# to give it that 'black and white' paper effect
 		warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-		#T = threshold_local(warped, 11, offset = 10, method = "gaussian")
-		#warped = (warped > T).astype("uint8") * 255
+		# T = threshold_local(warped, 11, offset = 5, method = "gaussian")
+		# warped = (warped > T).astype("uint8") * 255
+		# ret, warped = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 		# show the original and scanned images
+		contours, _ = cv2.findContours(warped, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		# Draw contours on original image
+		output = warped.copy()
+		#output = cv2.resize(output, (1750, 1350))
+		cv2.drawContours(output, contours, -1, (0, 255, 0), 2)
 		print("STEP 3: Apply perspective transform")
 		cv2.destroyAllWindows()
 		video.release()
-		cv2.imshow("Original", imutils.resize(orig, height = 650))
-		cv2.imshow("Scanned", cv2.resize(warped, (650, 502)))
+		cv2.imshow("Original", orig)
+		cv2.imshow("Scanned", output)
 		if cv2.waitKey(0) & 0xFF == ord('g'):		# adjust waitkey() to adjust frame rate
-			cv2.imwrite("c:\\Users\\Iris\\OneDrive - purdue.edu\\Documents\\School\\ME 274\\Piano Hand\\scan.jpg", imutils.resize(warped, height = 650))
-			break
+			warped = cv2.resize(warped, (1750, 1350))
+			cv2.imwrite("c:\\Users\\Iris\\OneDrive - purdue.edu\\Documents\\School\\Piano Hand\\twinkle.png", warped, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+			im = Image.open("c:\\Users\\Iris\\OneDrive - purdue.edu\\Documents\\School\\Piano Hand\\twinklE.png")
+			im.save("c:\\Users\\Iris\\OneDrive - purdue.edu\\Documents\\School\\Piano Hand\\twinkleDPI.png", dpi=(300, 300))
+		break
 	elif cv2.waitKey(33) &0xFF == ord('b'):
 		break
 video.release()
