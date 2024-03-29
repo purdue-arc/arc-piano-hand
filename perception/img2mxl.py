2b4491248b5aa443161d334a70b7b287b8c43cac
Anish Tiwari <anishtiwari425@gmail.com>
2024-03-28 20:09:00 -0400

    Update img2mxl.py
    working now

--- perception/img2mxl.py
+++ perception/img2mxl.py
@@ -1,16 +1,18 @@
 import subprocess
-
+import os 
 # Define the command
-command = r'C:\\"Program Files"\\Audiveris\\bin\\Audiveris.bat -batch -output "C:\\Users\\anish\\OneDrive\\Desktop" -export -- "C:\\Users\\anish\\OneDrive\\Desktop\\twinkle.png"'
-
+command = r'C:\\"Program Files"\\Audiveris\\bin\\Audiveris.bat -batch -output "C:\\Users\\anish\\OneDrive\\Desktop" -export -- "C:\\Users\\anish\\OneDrive\\Desktop\\twinkleDPI.pdf"'
+#command = ["C:\\'Program Files\'\\Audiveris\\bin\\Audiveris.bat", "-batch", "-output", "C:\\Users\\anish\\OneDrive\\Desktop","-export","--","C:\\Users\\anish\\OneDrive\\Desktop"]
+#command = r'ls'
 # Run the command in the terminal
-process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Wait for the process to finish and get the output and errors
-output, error = process.communicate()
-
+#output, error = process.communicate()
+#subprocess.run(["ls", "-l"])    
+os.system(command)
 # Print the output and errors
-if output:
-    print("Success")
-if error:
-    print("Failed")
+#if output:
+ #   print("Success")
+#if error:
+ #   print("Failed")
